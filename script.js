8a13f8382e7165ccf52c83f2b036498497695f4f
RicardoBachmann <ricardo.bachmann@icloud.com>
2022-11-07 13:34:17 +0100

    build buttons functionality

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ let homeScoreBtnTwo = document.getElementById("home-score-btn-2");
 let homeScoreBtnThree = document.getElementById("home-score-btn-3");
 let resetHomeScore = document.getElementById("reset-home-score");
 
+// Button Function - Home
+
+homeScoreBtnOne.addEventListener("click", function() {
+    homeScoreEl.textContent++;
+});
+
+homeScoreBtnTwo.addEventListener("click", function() {
+    homeScoreEl.textContent = Number(homeScoreEl.textContent) + 2;
+});
+
+homeScoreBtnThree.addEventListener("click", function() {
+    homeScoreEl.textContent = Number(homeScoreEl.textContent) + 3;
+});
+
+
+resetHomeScore.addEventListener("click", function() {
+    homeScoreEl.textContent = 0;
+})
